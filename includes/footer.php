f457ca541661d8b467841a3f19e828c8d393d8fa
johnahlenhed <johnahlenhed@gmail.com>
2025-12-15 10:19:52 +0100

    Minimal content on index

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,7 @@
 
+<footer>
+    <img src="/images/borta-bra-logo2.png" alt="Hotel Logo Footer" />
+    <p>&copy; <?php echo date('Y'); ?> Hotel Borta bra, hemma bäst!</p>
+</footer>
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,6 +6,47 @@ require_once __DIR__ . '/includes/header.php'; ?>
 
 <main>
     <p>Welcome to Hotel Borta bra, hemma bäst!</p>
+
+    <section>
+        <form>
+            <label type="date" name="checkin">
+                Check-in Date:
+            </label>
+
+            <label type="date" name="checkout">
+                Check-out Date:
+            </label>
+        </form>
+    </section>
+
+    <section>
+        <form method="POST" action="/book">
+            <fieldset>
+                <legend>Contact details</legend>
+                <label>
+                    Full name:
+                    <input type="text" name="name" required>
+                </label>
+
+                <label>
+                    Email:
+                    <input type="email" name="email" required>
+                </label>
+            </fieldset>
+
+            <fieldset>
+                <legend>Features</legend>
+                <label>
+                    <input type="checkbox" name="">
+                    <input type="checkbox" name="">
+                    <input type="checkbox" name="">
+                    <input type="checkbox" name="">
+                </label>
+            </fieldset>
+
+            <button type="submit">Book Now</button>
+        </form>
+    </section>
 </main>
 
 <?php require_once __DIR__ . '/includes/footer.php'; ?>
