37280d18444e988ed5505cd9c132c42d38455669
johnahlenhed <johnahlenhed@gmail.com>
2025-12-15 15:19:53 +0100

    Multiple room error handling

--- public/book.php
+++ public/book.php
@@ -19,6 +19,16 @@ $data = [
     'transfer_code' => $_POST['transfer_code'] ?? null,
 ];
 
+$selectedRooms = array_filter([
+    'economy' => $data['economy_checkin'],
+    'standard' => $data['standard_checkin'],
+    'luxury' => $data['luxury_checkin'],
+]);
+
+if (count($selectedRooms) != 1) {
+    exit('Please select only one room to book.');
+}
+
 $errors = bookingValidation::validateBookingData($data);
 
 if (!empty($errors)) {
