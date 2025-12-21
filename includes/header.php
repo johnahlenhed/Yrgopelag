2a46ee9f8a07f6d70d28f9712dade3cb378c8cfc
johnahlenhed <johnahlenhed@gmail.com>
2025-12-21 16:41:42 +0100

    Informal bolaget-page

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/public/css/styles.css">
     <link rel="stylesheet" href="/public/css/booking-calendar.css">
+    <link rel="stylesheet" href="/public/css/bolaget.css">
     <link rel="icon" href="/images/borta-bra-logo2.png">
     <title>Borta bra, hemma bäst!</title>
 </head>
@@ -14,6 +15,7 @@
 
     <header>
         <nav>
+            <a href="/public/bolaget.php">Bolaget</a>
             <a href="/public/index.php"><img src="/public/images/borta-bra-logo.png" alt="Hotel Logo" /></a>
             <a href="/public/admin/login.php">Login</a>
         </nav>
