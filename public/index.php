82778af87000827d15d1084dd3ba53cc7b43c94e
johnahlenhed <johnahlenhed@gmail.com>
2026-01-05 13:20:02 +0100

    Index hero styling

--- public/index.php
+++ public/index.php
@@ -26,13 +26,15 @@ require __DIR__ . '/../includes/header.php'; ?>
 
     <div class="welcome-container">
         <h1>Welcome to Borta bra, hemma bäst!</h1>
-        <div>
+        <div class="hero">
             <img src="/public/images/hotel.png" alt="full view of hotel">
-            <h3>A place to feel at home</h3>
-            <p><b>Welcome to the New Sweden Island Resort. We have gone to great lengths to bring the modest charm of a Falu-red cottage, just as the market demand is hinting at.</b></p>
-            <p>We hope you enjoy your stay with us, though we trust you will do so with appropriate moderation. Our island offers a serene environment, and we find that guests who manage their expectations—and their enthusiasm—tend to fit in best here.</p>
-            <p>We are ready to receive you. Please try to arrive on time; the tropical sun, unlike our patience, is quite consistent.</p>
-            <p>What are you waiting for? Book your room!</p>
+            <div class="welcome-text">
+                <h3>A place to feel at home</h3>
+                <p><b>Welcome to the New Sweden Island Resort. We have gone to great lengths to bring the modest charm of a Falu-red cottage, just as the market demand is hinting at.</b></p>
+                <p>We hope you enjoy your stay with us, though we trust you will do so with appropriate moderation. Our island offers a serene environment, and we find that guests who manage their expectations—and their enthusiasm—tend to fit in best here.</p>
+                <p>We are ready to receive you. Please try to arrive on time; the tropical sun, unlike our patience, is quite consistent.</p>
+                <p>What are you waiting for? Book your room!</p>
+            </div>
         </div>
     </div>
 
